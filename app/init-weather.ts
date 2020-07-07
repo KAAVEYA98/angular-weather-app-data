693dc57d707aa3b1309cf9ad309a3bb26f684a24
KAAVEYA98 <veyasugumar@gmail.com>
2020-07-08 02:43:39 +0530

    1 file modified

--- app/init-weather.ts
+++ app/init-weather.ts
@@ -3,21 +3,7 @@ export class Init {
     if (localStorage.getItem('weather-items') === null || localStorage.getItem('weather-items') === undefined) {
       console.log('Weather items not found!!!');
       const weatherItems = [
-        {
-          Cityname: 'Chennai',
-          description: 'RAINY',
-          temperature: 32
-        },
-        {
-          Cityname: 'Bangalore',
-          description: 'CLOUDY',
-          temperature: 30
-        },
-        {
-          Cityname: 'Delhi',
-          description: 'SUNNY',
-          temperature: 28
-        }
+        
       ];
       localStorage.setItem('weather-items', JSON.stringify(weatherItems));
     } else {
