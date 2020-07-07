ea688c8616a56593e2401d4e1d5f1e06673c9654
KAAVEYA98 <veyasugumar@gmail.com>
2020-07-08 02:44:30 +0530

    1 file modified

--- app/init-weather.ts
+++ app/init-weather.ts
@@ -2,9 +2,7 @@ export class Init {
   load() {
     if (localStorage.getItem('weather-items') === null || localStorage.getItem('weather-items') === undefined) {
       console.log('Weather items not found!!!');
-      const weatherItems = [
-        
-      ];
+      const weatherItems = {};
       localStorage.setItem('weather-items', JSON.stringify(weatherItems));
     } else {
       console.log('Loading weatherItems..');
